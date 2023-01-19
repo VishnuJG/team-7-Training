acf0fb5f0461f9a08bcc587ff7c2fe7b0e4fa361
VishnuJG <jgvishnu2001@gmail.com>
2023-01-19 22:02:46 +0530

    feat(search page): flask app for unbxd API connecting the frontend query operation
    The frontend mock server url is replaced with the API route for the flask app call. Now able to get
    product details directly from it.

---
+++ ProductQuery.py
@@ -0,0 +1,23 @@
+from flask import Flask, url_for, request, jsonify
+from datetime import datetime
+import requests
+import json
+from flask_cors import CORS
+
+app=Flask(__name__)
+CORS(app)
+
+
+@app.route('/product-query')
+def productQuery():
+    query_val = request.args.get('q', default="", type=str)
+    
+    unbxd_val = requests.get('https://search.unbxd.io/fb853e3332f2645fac9d71dc63e09ec1/demo-unbxd700181503576558/search?q={}'.format(query_val)).content
+    unbxd_val = json.loads(unbxd_val)
+    
+    return unbxd_val['response']['products']
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
