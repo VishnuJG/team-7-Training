094ba6c7d4008e484d23eb5b3772497a57ee5afb
Divya <divyashekar39@gmail.com>
2023-01-23 14:37:50 +0530

    docs(script): add comments in the database API
    add comments detailing arguments and return types in the various API methods

--- database/app.py
+++ database/app.py
@@ -4,14 +4,28 @@ from datetime import datetime
 import psycopg2
 import json 
 from queries import *
-import ast
 
+
+"""
+    Connect to the database
+    Args: None
+    Returns: connection
+"""
 
 def get_db_connection():
     conn = psycopg2.connect(host='localhost',database='unbxddatabase',user='unbxd',password='unbxd')
     return conn
 
+
 app = Flask(__name__)
+
+
+"""
+    Popoulate the databse with json objects. If tables do not exist, first create them according to a schema
+    Args: List of JSON objects 
+          keys: ['uniqueId', 'title', 'price', 'productDescription', 'productImage', 'catlevel1Name, 'catlevel2Name']
+    Returns: None
+"""
 
 @app.route('/product-details', methods=['POST'])
 def populate_db():
@@ -20,9 +34,13 @@ def populate_db():
     # print(data)
     conn = get_db_connection()
     cur = conn.cursor()
+
+    # Create tables in the database
     cur.execute(CREATE_PRODUCT_TABLE)
     cur.execute(CREATE_CATEGORY_TABLE)
     product_no = 0
+
+    # For each JSOn object, retrieve values for all keys
     for i in data: 
         # print("IIIIIII", i)
         uniqueId=i['uniqueId']
@@ -37,38 +55,59 @@ def populate_db():
 
         #Assign category ID
         catlevel1Name = i['catlevel1Name']
-        catlevel2Name = i['catlevel2Name']
+        try:
+            catlevel2Name = i['catlevel2Name']
+        except:
+            catlevel2Name = " "
 
         #Remove spaces in the category names
         catlevel1Name = catlevel1Name.replace(" ", "")
         catlevel2Name = catlevel2Name.replace(" ", "")
+
+        # Check if category ID for a hierarchy of cetegories already exists in category table
         cur.execute(CATEGORY_ID_EXISTS, (catlevel2Name, catlevel1Name,))
         category_flag = cur.fetchone()[0]
+
+        # Create a new category ID in category table if not already exists
         if category_flag == False:
             cur.execute(INSERT_CATEGORY_ID, (catlevel2Name, catlevel1Name,))
+
+        # Look up category ID for current hierarchy 
         cur.execute(GET_CATEGORY_ID, (catlevel2Name, catlevel1Name,))
         category_id = cur.fetchone()[0]
 
+        # Insert details of current product into product table
         cur.execute(INSERT_PRODUCT, (uniqueId, title, description, price, img_url, category_id))
         print("Product {} inserted", product_no+1)
-    cur.execute(SELECT_ALL_PRODUCTS)
-    prods = cur.fetchall()
-    for i in prods:
-        print (i)
+
+    # cur.execute(SELECT_ALL_PRODUCTS)
+    # prods = cur.fetchall()
+    # for i in prods:
+    #     print (i)
     conn.commit()
     cur.close()
     conn.close()
     return "completed"
 
 
+""" 
+    Retrieve product details from database for a single product identified by a unique ID
+    Args: str uniqueId
+    Returns: dict product_details
+"""
+
 @app.route('/product-details', methods=['GET'])
 def get_product_details():
+    # Parse arguments from request 
     uniqueId = str(request.args.get("uniqueId"))
-    product_details = {}
     conn = get_db_connection()
     cur = conn.cursor()
+
+    # Find corresponding product details from product table
     cur.execute(GET_PRODUCT, (str(uniqueId),))
     query_response = cur.fetchall()
+
+    product_details = {}
     
     try:
         title = query_response[0][1]
@@ -83,6 +122,7 @@ def get_product_details():
         price = " "
         imageurl = " "
     
+    # Create JSON object as response
     product_details['title'] = title
     product_details['description'] = description
     product_details['price'] = price
@@ -91,8 +131,17 @@ def get_product_details():
     return json.dumps(product_details)
 
 
+"""
+    Filters and retrieves all products as a list of JSOn objects belonging to a hierarchy of categories from the database
+    Args: str catlevel1Name
+          str catlevel2Name
+    Returns: list product_details
+"""
+
 @app.route('/category', methods=['GET'])
 def get_category_products():
+
+    # Parse arguments from request 
     catlevel1Name = request.args.get('catlevel1Name')
     catlevel2Name = request.args.get('catlevel2Name')
 
@@ -103,26 +152,29 @@ def get_category_products():
     category_products = []
     conn = get_db_connection()
     cur = conn.cursor()
-    cur.execute(GET_CATEGORY_ID, (catlevel2Name, catlevel1Name  ))
-    print(catlevel2Name, catlevel1Name)
-    
+
+    # Lookup category ID of corresponding hierarchy from category table
+    cur.execute(GET_CATEGORY_ID, (catlevel2Name, catlevel1Name))
+    # print(catlevel2Name, catlevel1Name)
     category_id = cur.fetchone()[0]
 
+    # Check if the given hierarchy exists in database
     if type(category_id) == "NoneType":
         print("Invalid category")
     
-    
+    # Retrieve all rows having respective category ID from product table
     cur.execute(GET_CATEGORY_PRODUCTS, (category_id,))
     for product in cur.fetchall():
         # print(product)
         product_details = {}
         product_details['title'] = product[1]
-        product_details['description'] = product[2]
+        # product_details['description'] = product[2]
         product_details['price'] = product[3]
         product_details['imageurl'] = product[4]
         print(product_details)
         category_products.append(product_details)
-    print(category_products)
+
+    # print(category_products)
     return json.dumps(product_details)
 
 
@@ -130,9 +182,11 @@ def get_category_products():
 def delete(id):
     pass
 
+
 @app.route('/update/<string:id>', methods=['GET', 'POST'])
 def update(id):
     pass
+
 
 if __name__ == "__main__":
     app.run(debug=True)
