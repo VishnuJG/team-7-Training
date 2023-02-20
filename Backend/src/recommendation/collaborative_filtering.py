19e0170de8ae501498bd2239b15b1be4e1cdc4cc
Divya <divyashekar39@gmail.com>
2023-02-20 15:56:24 +0530

    feat(Recommendation): add collaborative filtering
    add collaborative filtering based on user-item interaction

--- Backend/src/recommendation/collaborative_filtering.py
+++ Backend/src/recommendation/collaborative_filtering.py
@@ -27,24 +27,59 @@ model.fit(trainset)
 
 # Predict ratings on the test set
 predictions = model.test(testset)
+print(predictions)
 
 # Compute RMSE on the test set
 rmse = accuracy.rmse(predictions)
 # print("RMSE", rmse)
 
 # Recommend items to a user
-user_id = '7'
-num_items = 4
-user_items = [i[0] for i in trainset.ur[int(user_id)]]
-all_items = list(model.trainset.ir.keys())
-unseen_items = set(all_items) - set(user_items)
-# print(unseen_items)
-# print(data.all_items())
-testset = [[user_id, item_id, 4.] for item_id in unseen_items]
-predictions = model.test(testset)
-# print(sorted(predictions, key=lambda x: x.est, reverse=True))
-top_n = sorted(predictions, key=lambda x: x.est, reverse=True)[:num_items]
-# for pred in top_n:
-#     print(map_to_productID(mapping_dict, pred.iid))
-print([pred.iid for pred in top_n])
-print([map_to_productID(mapping_dict, pred.iid) for pred in top_n])
+# user_id = '7'
+# num_items = 4
+# user_items = [i[0] for i in trainset.ur[int(user_id)]]
+# all_items = list(model.trainset.ir.keys())
+# unseen_items = set(all_items) - set(user_items)
+# # print(unseen_items)
+# # print(data.all_items())
+# testset = [[user_id, item_id, 4.] for item_id in unseen_items]
+# predictions = model.test(testset)
+# # print(sorted(predictions, key=lambda x: x.est, reverse=True))
+# top_n = sorted(predictions, key=lambda x: x.est, reverse=True)[:num_items]
+# # for pred in top_n:
+# #     print(map_to_productID(mapping_dict, pred.iid))
+# print([pred.iid for pred in top_n])
+# print([map_to_productID(mapping_dict, pred.iid) for pred in top_n])
+
+def get_Iu(uid):
+    """ return the number of items rated by given user
+    args: 
+      uid: the id of the user
+    returns: 
+      the number of items rated by the user
+    """
+    try:
+        return len(trainset.ur[trainset.to_inner_uid(uid)])
+    except ValueError: # user was not part of the trainset
+        return 0
+    
+def get_Ui(iid):
+    """ return number of users that have rated given item
+    args:
+      iid: the raw id of the item
+    returns:
+      the number of users that have rated the item.
+    """
+    try: 
+        return len(trainset.ir[trainset.to_inner_iid(iid)])
+    except ValueError:
+        return 0
+    
+df = pd.DataFrame(predictions, columns=['uid', 'iid', 'rui', 'est', 'details'])
+df['Iu'] = df.uid.apply(get_Iu)
+df['Ui'] = df.iid.apply(get_Ui)
+df['err'] = abs(df.est - df.rui)
+
+# Get top 10 recommendations
+best_predictions = df.sort_values(by='err')[:10]
+worst_predictions = df.sort_values(by='err')[-10:]
+# print(best_predictions)
